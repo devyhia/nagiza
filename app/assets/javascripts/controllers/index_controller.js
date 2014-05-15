23e8aa04a65e4acdfb4e22a14bd9537193dd5e8e
Yehia Abo El-Nga <yehia@devrok.com>
2014-05-15 19:16:18 +0200

    Finish Simulator, Assembler & Labels

--- app/assets/javascripts/controllers/index_controller.js
+++ app/assets/javascripts/controllers/index_controller.js
@@ -1,18 +1,85 @@
 // for more details see: http://emberjs.com/guides/controllers/
+function _simulate(controller, time, program) {
+	var pc = parseInt(controller.get('pc'));
+	window.editor.gotoLine(pc+1);
+	execute(controller, program[pc]);
+	var stall = parseInt(controller.get('stall'));
+	if(pc < program.length && !controller.get('break')) {
+		setTimeout(function(){
+			controller.set('stall', 0);
+			_simulate(controller, time, program);
+		}, stall ? stall : time);
+	} else controller.set('running', false);
+}
 
 Nagiza.IndexController = Ember.Controller.extend({
 	actions: {
 		newCode: function() {
 			// Create & Initialize Registers
 			console.log('newCode');
-			var registers = [];
+			var registers = Em.A([]);
 			for (var i = 0; i < 32; i++) {
-				registers.push({id: i, value: 0});
+				registers.pushObject(Em.Object.create({id: i, value: 0}));
 			}
 
 			console.log(registers);
 
 			this.set('registers', registers);
+
+			// Create & Initialize Memory
+			this.set('memory', Em.Object.create());
+
+			// Create Assembly Code
+			this.set('assmebly', Em.A([]));
+
+			this.set('zflag', false);
+			this.set('pc', 0);
+			this.set('last_key');
+			this.set('clk', 10);
+			this.set('stall', 0);
+
+			this.set('break', false);
+			this.set('running', false);
+		},
+
+		simulate: function() {
+			var controller = this;
+			
+			this.set('pc', 0);
+			this.set('running', true);
+			this.set('break', false);
+
+			var time = (1/parseInt(this.get('clk')))*1000; 
+			var program = window.editor.getValue().split('\n');
+			_simulate(controller, time, program);
+		},
+
+		assemble: function() {
+			var controller = this;
+			controller.set('assembly', Em.A([]));
+			var labels = Em.Object.create();
+			var cnt = 0;
+			window.editor.getValue().split('\n').forEach(function(line) {
+				if(/^(\w+)\:/i.test(line)) {
+					var temp = /^(\w+)\:/i.exec(line);
+					labels.set(temp[1], cnt);
+				}
+				cnt++;
+			});
+			controller.set('labels', labels);
+			window.labels = labels;
+			console.log(controller.get('labels'));
+			window.editor.getValue().split('\n').forEach(function(line) {
+				assemble(controller, line);
+			});
+		},
+
+		storeKey: function(key) {
+			this.set('last_key', key);
+		}, 
+
+		breakRunning: function() {
+			this.set('break', true);
 		}
 	}
 });
--- app/assets/javascripts/views/index_view.js
+++ app/assets/javascripts/views/index_view.js
@@ -1,10 +1,25 @@
 // for more details see: http://emberjs.com/guides/views/
-
+var _key_mappings = {
+	"38": 2, // UP
+	"40": 4, // DOWN
+	"39": 3, // RIGHT
+	"37": 5 // LEFT
+};
 Nagiza.IndexView = Ember.View.extend({
   templateName: 'index',
   didInsertElement: function () {
-	var editor = ace.edit("editor");
-	editor.setTheme("ace/theme/xcode");
-	editor.getSession().setMode("ace/mode/assembly_x86");
+	window.editor = ace.edit("editor");
+	window.editor.setTheme("ace/theme/xcode");
+	window.editor.getSession().setMode("ace/mode/assembly_x86");
+	var controller = this.get('controller');
+	window.editor.getSession().on('change', function(e) {
+	   controller.send('assemble');
+	});
+
+	$(document).keyup(function(e) {
+		if([37,38,39,40].contains(e.keyCode)) {
+			controller.send('storeKey', _key_mappings[e.keyCode.toString()]);
+		}
+	});
   }
 });
